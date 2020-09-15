d409597ac540b9ef6fd1475f9a03b0cec1815a24
NeonilaKravtcova <kravtcovaneonila@gmail.com>
2020-09-15 09:54:57 +0200

    HW_05 Project Person fixed and updated

--- 20200911_Person/src/de/telran/application/PersonApp.java
+++ 20200911_Person/src/de/telran/application/PersonApp.java
@@ -5,19 +5,57 @@ import de.telran.data.Person;
 public class PersonApp {
 
     public static void main(String[] args) {
-        Person person1 = new Person("Vasya", 25);
+        Person person1 = new Person("Vasya", 25, 178);
         person1.setName("Vasiliy");
-        Person person2 = new Person("Alex", 24);
-        Person person3 = new Person("Katya", 21);
-        Person person4 = new Person("Maria Antoinetta", 23);
+        Person person2 = new Person("Alex", 24, 182);
+        Person person3 = new Person("Katya", 21, 168);
+        Person person4 = new Person("Maria Antoinette", 23, 164);
+        //Person person5 = new Person("Alex", 24, 182);//для проверки работы equals
+        Person person5 = new Person("Alex", 28, 175);
 
-        Person[] people = {person1, person2, person3, person4};
+        Person[] people = {person1, person2, person3, person4, person5};
+
+        //System.out.println(person2 == person5);//-> false - так как одинаковые объекты лежат в разных местах
+
+        //System.out.println(person2.equals(person5));//-> будет true (так как объекты идентичны) при условии,
+        // что мы переопределили метод equals так, (@Override)
+        // что он будет сравнивать сами объекты.
+        // Если мы не переопределяем сами метод equals,
+        // то будет false, так как он изначально будет сравнивать ссылки
+
+        // Если мы переопределяем equals, то должны переопределить и hashcode. Если нам не нужно сравнивать объекты,
+        // то можем вообще не переопределять эти два метода.
+
+        /*int a = 2;
+        int b = 2;
+        boolean c = a == b;
+        System.out.println(c);*/
 
         Person.outPrint(people);
 
         Person.findOldestPerson(people);
 
         Person.findLongestNamePerson(people);
+
+        Person.sortPersonsByAge(people);
+
+        Person.outPrint(people);
+
+        Person.sortPersonsByAge2(people);
+
+        Person.outPrint(people);
+
+        Person.sortPersonsByNames(people);
+
+        Person.outPrint(people);
+
+        Person.sortPersonsByHeight(people);
+
+        Person.outPrint(people);
+
+        Person.sortPersonsByHeight2(people);
+
+        Person.outPrint(people);
 
     }
 
--- 20200911_Person/src/de/telran/data/Person.java
+++ 20200911_Person/src/de/telran/data/Person.java
@@ -1,16 +1,15 @@
 package de.telran.data;
 
-import java.util.Arrays;
-import java.util.Comparator;
-
 public class Person {
     private String name;
     private int age;
+    private int height;
 
     //Конструктор
-    public Person(String name, int age) {
+    public Person(String name, int age, int height) {
         this.name = name;
         this.age = age;
+        this.height = height;
     }
 
     public String getName() {
@@ -26,7 +25,6 @@ public class Person {
     }
 
     public void setAge(int age) {
-        //В этом методе можем сделать защиту от неправильных данных
         if (age > 0 && age < 110) {
             this.age = age;
         } else {
@@ -34,10 +32,43 @@ public class Person {
         }
     }
 
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        if (height > 0 && height < 230) {
+            this.height = height;
+        } else {
+            System.out.println("Wrong height!");
+        }
+    }
+
+/*    @Override//Это аннотация о том, что метод переписан
+    public boolean equals(Object o) {
+        if (this == o) return true;//Если ссылка в памяти указывает на одни и те же поля,
+        // то объекты идентичны и дальше проверять не надо
+        if (!(o instanceof Person)) return false;
+
+        Person person = (Person) o;
+
+        if (getAge() != person.getAge()) return false;
+        return getName().equals(person.getName());
+    }
+
+    @Override
+    //Фундаментальный метод класса Object, используется для генерации целочисленного кода объекта
+    public int hashCode() {
+        int result = getName().hashCode();
+        result = (31 * result) + getAge();
+        return result;
+    }*/
+
     @Override
     public String toString() {
         return "Name: " + name +
-                ", age " + age + "\n";
+                ", age " + age +
+                ", height " + height + "\n";
     }
 
     public static void outPrint(Person[] array) {
@@ -47,24 +78,128 @@ public class Person {
         System.out.println();
     }
 
-    //public - так как будем использовать метод не внутри класса
-    //static - так как будем перебирать группу людей
     public static void findOldestPerson(Person[] array) {
-        Arrays.sort(array, Comparator.comparing(Person::getAge));
-        System.out.println("The oldest person is " + array[array.length - 1].getName() + ", he is "
-                + array[array.length - 1].getAge() + " years old. \n");
+        //Сначала написала сама через count
+/*        int count = 1;
+        Person oldestPerson = array[0];
+        for (int i = 0; i < array.length - 1; i++) {
+            if (oldestPerson.getAge() < array[count].getAge()) {
+                oldestPerson = array[count];
+            }
+            count++;
+        }*/
+        // Потом увидела ваше решение в классе, оставляю его )
+        Person oldestPerson = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (oldestPerson.getAge() < array[i].getAge()) {
+                oldestPerson = array[i];
+            }
+        }
+        System.out.println("The oldest person is " + oldestPerson.getName() + ", "
+                + oldestPerson.getAge() + " years old. \n");
     }
 
     public static void findLongestNamePerson(Person[] array) {
-        Person temp;
         for (int i = 0; i < array.length - 1; i++) {
             if (array[i].getName().length() > array[i + 1].getName().length()) {
-                temp = array[i];
+                Person temp = array[i];
                 array[i] = array[i + 1];
                 array[i + 1] = temp;
             }
         }
         System.out.println("A person with the longest name is " + array[array.length - 1].getName() + ", "
-                + array[array.length - 1].getAge() + " years old.");
+                + array[array.length - 1].getAge() + " years old. \n");
+    }
+    //Компаратор - метод сравнения двух людей по возрасту (двух объектов по полю возраст)
+    //static - метод не зависит от объекта
+    private static int comparePersonsAge(Person p1, Person p2){
+        if (p1.age > p2.age){
+            return 1;
+        } else if (p1.age < p2.age) {
+            return -1;
+        } return 0;
+    }
+
+    //Теперь создадим не статичный компаратор для объектов
+    private int comparePersonsByAge(Person p){
+        if (this.age > p.age){
+            return 1;
+        } else if (this.age < p.age){
+            return -1;
+        } return 0;
+    }
+
+    //Теперь при помощи компаратора делаем сортировку bubblesort
+    public static void sortPersonsByAge(Person[] array){
+        System.out.println("Сортировка по возрасту по убыванию: ");
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 0; j < (array.length - 1) - i; j++) {
+                if (comparePersonsAge(array[j], array[j + 1]) < 0){
+                    Person temp = array[j + 1];
+                    array[j + 1] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+    }
+
+    public static void sortPersonsByAge2(Person[] array){
+        System.out.println("Сортировка по возрасту по возрастанию: ");
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 0; j < (array.length - 1) - i; j++) {
+                if (array[j].comparePersonsByAge(array[j + 1]) > 0){
+                    Person temp = array[j + 1];
+                    array[j + 1] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+    }
+
+    public static void sortPersonsByNames(Person[] array){
+        System.out.println("Сортировка по имени по возрастанию: ");
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 0; j < array.length - 1 - i; j++) {
+                if (array[j].name.compareTo(array[j + 1].name) > 0){
+                    Person temp = array[j + 1];
+                    array[j + 1] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+    }
+
+    private int comparePersonsByHeight(Person p){
+        if (this.height > p.height){
+            return 1;
+        } else if (this.height < p.height){
+            return -1;
+        } return 0;
+    }
+
+    public static void sortPersonsByHeight(Person[] array){
+        System.out.println("Сортировка по весу по возрастанию: ");
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 0; j < (array.length - 1) - i; j++) {
+                if (array[j].comparePersonsByHeight(array[j + 1]) > 0){
+                    Person temp = array[j + 1];
+                    array[j + 1] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+    }
+
+    public static void sortPersonsByHeight2(Person[] array){
+        System.out.println("Сортировка по весу по убыванию: ");
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 0; j < (array.length - 1) - i; j++) {
+                if (array[j].comparePersonsByHeight(array[j + 1]) < 0){
+                    Person temp = array[j + 1];
+                    array[j + 1] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
     }
 }
