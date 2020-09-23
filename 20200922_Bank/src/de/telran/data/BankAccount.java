8d724ac98a9204663ff757a88453aa3a1d928146
NeonilaKravtcova <kravtcovaneonila@gmail.com>
2020-09-24 00:10:13 +0200

    HW_10 Project Bank

---
+++ 20200922_Bank/src/de/telran/data/BankAccount.java
@@ -0,0 +1,69 @@
+package de.telran.data;
+
+public class BankAccount {
+    private int account;
+    private Person client;
+    private double balance;
+    private static int accountsNumber = 10100000;
+
+    public BankAccount(Person client, double balance){
+        account = ++accountsNumber;
+        this.client = client;
+        this.balance = balance;
+    }
+
+    public int getAccount() {
+        return account;
+    }
+
+    public Person getClient() {
+        return client;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof BankAccount)) return false;
+
+        BankAccount that = (BankAccount) o;
+
+        if (getAccount() != that.getAccount()) return false;
+        if (Double.compare(that.getBalance(), getBalance()) != 0) return false;
+        return getClient().equals(that.getClient());
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        result = getAccount();
+        result = 31 * result + getClient().hashCode();
+        temp = Double.doubleToLongBits(getBalance());
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Bankaccount number: " +
+                account +
+                "\n" + client.toStringName() +
+                "\nBalance: " + balance;
+    }
+
+    public void debit(double sum){
+        balance = balance + sum;
+    }
+
+    public boolean credit(double sum){
+        if (sum <= balance){
+            balance = balance - sum;
+            return true;
+        }
+        return false;
+    }
+}
---
+++ 20200922_Bank/src/de/telran/data/Person.java
@@ -0,0 +1,82 @@
+package de.telran.data;
+
+import java.util.Date;
+import java.util.Objects;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int birthDay;
+    private String address;
+    private Sex sex;
+
+    public Person(String firstName, String lastName, int birthDay, String address, Sex sex) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDay = birthDay;
+        this.address = address;
+        this.sex = sex;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getBirthDay() {
+        return birthDay;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public Sex getSex() {
+        return sex;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Person)) return false;
+
+        Person person = (Person) o;
+
+        if (getBirthDay() != person.getBirthDay()) return false;
+        if (!getFirstName().equals(person.getFirstName())) return false;
+        if (!getLastName().equals(person.getLastName())) return false;
+        if (!getAddress().equals(person.getAddress())) return false;
+        return getSex() == person.getSex();
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getFirstName().hashCode();
+        result = 31 * result + getLastName().hashCode();
+        result = 31 * result + getBirthDay();
+        result = 31 * result + getAddress().hashCode();
+        result = 31 * result + getSex().hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Client: " +
+                firstName +
+                " " + lastName + '\'' +
+                ", birthDay: " + birthDay +
+                ", address: " + address +
+                ", sex: " + sex;
+    }
+
+    public String toStringName(){
+        return "Client: " + firstName + " " + lastName;
+    }
+}
