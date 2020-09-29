ea26dfd32308da80a73cabdef4c1c84319bc6733
NeonilaKravtcova <69136715+NeonilaKravtcova@users.noreply.github.com>
2020-09-29 22:37:37 +0200

    Update EnumApp.java

--- 20200917_EnumProjectWeek/src/de/telran/application/EnumApp.java
+++ 20200917_EnumProjectWeek/src/de/telran/application/EnumApp.java
@@ -4,9 +4,10 @@ import de.telran.data.Schedule;
 
     public class EnumApp {
         public static void main(String[] args) {
-            DayOfWeek[] days = {DayOfWeek.MONDAY, DayOfWeek.TUESDAY, DayOfWeek.WEDNESDAY, DayOfWeek.THURSDAY, DayOfWeek.FRIDAY, DayOfWeek.SATURDAY, DayOfWeek.SUNDAY};
-
-            Schedule schedule = new Schedule(days);
+            //DayOfWeek[] days = {DayOfWeek.MONDAY, DayOfWeek.TUESDAY, DayOfWeek.WEDNESDAY, DayOfWeek.THURSDAY, DayOfWeek.FRIDAY, DayOfWeek.SATURDAY, DayOfWeek.SUNDAY};
+            //Schedule schedule = new Schedule(days); - заменим эти две строки на строку 10
+            
+            Schedule schedule = new Schedule(DayOfWeek.value());//Метод, который возвращает массив объектов enum 
             schedule.wakeUp(DayOfWeek.THURSDAY);
             schedule.wakeUp(DayOfWeek.SUNDAY);
             schedule.displayTodayMenu(DayOfWeek.MONDAY);
