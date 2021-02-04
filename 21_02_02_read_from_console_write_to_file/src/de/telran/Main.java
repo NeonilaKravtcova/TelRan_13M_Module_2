66eae98d4ad592adf39a2857f0b004e42855fde1
NeonilaKravtcova <kravtcovaneonila@gmail.com>
2021-02-04 01:45:24 +0100

    21_02_02_read_from_console_write_to_file updated with solid

--- 21_02_02_read_from_console_write_to_file/src/de/telran/Main.java
+++ 21_02_02_read_from_console_write_to_file/src/de/telran/Main.java
@@ -6,20 +6,44 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        readFromConsoleAndWriteToFile("text.txt");
+        //readFromConsoleAndWriteToFile("text.txt");
+
+        List<String> text = readFromConsole();
+        writeToFile("text.txt", text);
     }
 
-    public static void readFromConsoleAndWriteToFile(String fileName) throws IOException {
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+    public static List<String> readFromConsole() throws IOException{
+        try(BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))){
             List<String> res = new ArrayList<>();
             String line;
-            while ((line = br.readLine()) != null && !line.equals("exit")) {
-                res.add(line + "\n");
+            while ((line = reader.readLine()) != null && !line.equals("exit")){
+                res.add(line);
             }
-            try (FileWriter writer = new FileWriter(fileName)) {
-                for (String str : res) {
-                    writer.write(str);
-                }
+            return res;
+        }
+    }
+
+    public static void writeToFile(String file, List<String> text) throws FileNotFoundException {
+        try(PrintWriter pw = new PrintWriter(file)){
+            for (String line : text){
+                pw.println(line);
+            }
+        }
+    }
+
+    //Мой метод. Не соблюдается принцип единой ответственности.
+    public static void readFromConsoleAndWriteToFile(String fileName) throws IOException {
+        List<String> res = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            String line;
+            while ((line = br.readLine()) != null && !line.equals("exit")) {
+                res.add(line);
+            }
+        }
+        try (FileWriter writer = new FileWriter(fileName)) {
+            for (String str : res) {
+                writer.write(str);
+                writer.write("\n");
             }
         }
     }
