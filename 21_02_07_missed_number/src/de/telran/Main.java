8e60769f663daeb0b2b70f4347ca3c66f319fc4c
NeonilaKravtcova <kravtcovaneonila@gmail.com>
2021-02-09 19:17:46 +0100

    21_02_07_missing number 2 methods and tests

--- 21_02_07_missed_number/src/de/telran/Main.java
+++ 21_02_07_missed_number/src/de/telran/Main.java
@@ -1,14 +1,35 @@
 package de.telran;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         List<Integer> list = createArray(100);
         System.out.println(list);
         System.out.println(findMissingNumber(list));
+        System.out.println();
+
+        System.out.println(findMissingNumber2(list));
+    }
+
+    public static int findMissingNumber(List<Integer> input){
+        int sum = 0;
+        for (Integer i : input) {
+            sum += i;
+        }
+        int n = input.size() + 1;
+        return n * (n + 1) / 2 - sum;
+    }
+
+    public static int findMissingNumber2(List<Integer> input){
+        List<Integer> inputCopy = new ArrayList<>(input);
+        Collections.sort(inputCopy);
+        for (int i = 0; i <= inputCopy.size(); i++) {
+            if (inputCopy.get(i) != i + 1){
+                return i + 1;
+            }
+        }
+        return 0;
     }
 
     public static List<Integer> createArray(int n){
@@ -19,14 +40,5 @@ public class Main {
         array.remove((int) (Math.random() * (n - 1)));
         Collections.shuffle(array);
         return array;
-    }
-
-    public static int findMissingNumber(List<Integer> input){
-        int sum = 0;
-        for (Integer i : input) {
-            sum += i;
-        }
-        int n = input.size() + 1;
-        return n * (n + 1) / 2 - sum;
     }
 }
