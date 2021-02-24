5212514986aeb3df7cd0c517778b5e07497dedc3
NeonilaKravtcova <kravtcovaneonila@gmail.com>
2021-02-24 22:40:14 +0100

    21_02_24 employees counter

---
+++ 21_02_24_employee/src/de/telran/Department.java
@@ -0,0 +1,24 @@
+package de.telran;
+
+import java.util.List;
+
+public class Department {
+
+    private String name;
+    private String code;
+    private List<Employee> employees;
+
+    public Department(String name, String code, List<Employee> employees) {
+        this.name = name;
+        this.code = code;
+        this.employees = employees;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+}
